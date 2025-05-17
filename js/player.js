a107717ef5d9e37d792ad6bac4f9df762755886e
raresperta <40494938+raresperta@users.noreply.github.com>
2025-05-17 14:41:38 +0300

    gata. mai trebuie doar modificarile de text legate socials si facut playlist-ul

--- js/player.js
+++ js/player.js
@@ -4,12 +4,16 @@ function toggleIframeHeight() {
   const iframe = document.getElementById("myIframe");
   const extend_button = document.querySelector(".extend_button");
 
-  if (isExpanded) {
+  if (!isExpanded) {
     iframe.style.height = "80px"; // original height
-    extend_button.innerHTML='<img src="./img/up.png">'
+    extend_button.innerHTML='<img src="./img/up.png">';
+    document.body.style.overflow = "";
+    document.documentElement.style.overflow = "";
 } else {
-    iframe.style.height = "50vh"; // expanded height (or any other value)
+    iframe.style.height = "70vh"; // expanded height (or any other value)
     extend_button.innerHTML='<img src="./img/down.png">'
+    document.body.style.overflow = "hidden";
+    document.documentElement.style.overflow = "hidden";
 }
 
   isExpanded = !isExpanded;
